58ec56371bdd6107c6827e730fd33adb5dbbf946
jungwang <jklkwang0727@gmail.com>
2023-07-05 08:56:57 +0900

    Add getSum function

---
+++ CRA_TDD/cal.cpp
@@ -0,0 +1,6 @@
+
+
+int getSum(int a, int b)
+{
+	return (a + b);
+}
--- CRA_Test/test.cpp
+++ CRA_Test/test.cpp
@@ -1,6 +1,18 @@
 #include "pch.h"
+#include "../CRA_TDD/cal.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+
+TEST(cal, add)
+{
+	for(int i = 0; i < 10; ++i)
+	{
+		int a = rand() % 1000;
+		int b = rand() % 1000;
+		EXPECT_EQ(getSum(a, b), (a + b));
+	}
 }
