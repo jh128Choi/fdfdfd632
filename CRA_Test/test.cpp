1b1a992ed4f53258331347a6453ce9299eb44a30
JunhyeokChoi <anchovy1228@naver.com>
2023-07-05 09:25:35 +0900

    getMinus 구현하였습니다

--- CRA_Test/test.cpp
+++ CRA_Test/test.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
+#include "../CRA_TDD/cal.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(CalTest, Minus) {
+	int ret = getMinus(5, 3);
+	EXPECT_EQ(2, ret);
 }
